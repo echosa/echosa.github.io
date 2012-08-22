6831ed32fb359a0a3eeff625fadd336dbadd6cc6
Brian Zwahr <echosa@gmail.com>
2012-08-22 09:45:21 -0500

    Added blog post and updated github plugin to work with API v3.

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -10,26 +10,27 @@ var github = (function(){
   return {
     showRepos: function(options){
       $.ajax({
-          url: "http://github.com/api/v2/json/repos/show/"+options.user+"?callback=?"
+          url: "https://api.github.com/users/"+options.user+"/repos?callback=?"
         , type: 'jsonp'
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
-          if (!data || !data.repositories) { return; }
-          for (var i = 0; i < data.repositories.length; i++) {
-            if (options.skip_forks && data.repositories[i].fork) { continue; }
-            repos.push(data.repositories[i]);
-          }
-          repos.sort(function(a, b) {
-            var aDate = new Date(a.pushed_at).valueOf(),
-                bDate = new Date(b.pushed_at).valueOf();
+            if (!data || !data.data) { return; }
+            for (var i = 0; i < data.data.length; i++) {
+                var currentRepo = data.data[i];
+                if (options.skip_forks && currentRepo.fork) { continue; }
+                repos.push(currentRepo);
+            }
+            repos.sort(function(a, b) {
+                var aDate = new Date(a.pushed_at).valueOf(),
+                    bDate = new Date(b.pushed_at).valueOf();
 
-            if (aDate === bDate) { return 0; }
-            return aDate > bDate ? -1 : 1;
-          });
+                if (aDate === bDate) { return 0; }
+                return aDate > bDate ? -1 : 1;
+            });
 
-          if (options.count) { repos.splice(options.count); }
-          render(options.target, repos);
+            if (options.count) { repos.splice(options.count); }
+            render(options.target, repos);
         }
       });
     }
